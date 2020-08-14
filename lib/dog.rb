271783e90fe279ee66095532b432824a8be512dd
danainjax <danainjax@gmail.com>
2020-08-14 19:55:11 -0400

    changes to puppy

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,15 @@
-# Add your code here
+class Dog
+
+    @@all = []
+
+    attr_accessor :name
+    def initialize(name)
+        @name= name
+    end
+
+    def self.all
+        @@all
+    end
+    
+
+end
